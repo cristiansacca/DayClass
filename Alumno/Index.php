07e2e0db39214a363e94bfc85785f557ece9990e
Cristian Sacca <cristiansacca97@gmail.com>
2020-07-12 04:01:43 -0300

    Arreglos index alumnos y cambios menores

---
+++ Alumno/Index.php
@@ -0,0 +1,27 @@
+<?php
+include "../header.html";
+?>
+<link href="../DayClassStyle.css" rel="stylesheet" type="text/css">
+
+<div class="container text-center">
+    <div class="form-row justify-content-center">
+        <div class=" form-group col-md-4 border m-4 p-5 table-danger">
+            <h3>Mis cursos</h3>
+        </div>     
+        <div class=" form-group col-md-4 border m-4 p-5 table-primary">
+            <h3>Justificativos</h3>
+        </div>
+    </div>
+    <div class="form-row justify-content-center">
+        <div class=" form-group col-md-4 border m-4 p-5 table-success">
+            <h3>Dar presente</h3>
+        </div>     
+        <div class=" form-group col-md-4 border m-4 p-5 table-warning">
+            <h3>Mis asistencias</h3>
+        </div>
+    </div>
+</div>
+
+<?php
+include "../footer.html";
+?>
