5b4e030ce1420d978d789e9d9a2fd7111587877f
Cristian Sacca <cristiansacca97@gmail.com>
2020-07-27 19:44:09 -0300

    Genera tabla de alumnos desde la BD

--- Administrador/config_alumno.php
+++ Administrador/config_alumno.php
@@ -1,21 +1,27 @@
 <?php
 include "../header.html";
+include "../databaseConection.php";
+
+$consulta1 = $con->query("SELECT * FROM alumno");
+
 ?>
 <script src="administrador.js"></script>
 
-
-
-<style>.custom-file-label::after { content: "Elegir";}</style>
+<style>
+    .custom-file-label::after {
+        content: "Elegir";
+    }
+</style>
 <div class="container">
     <h1 class="display-4 my-2">Alumnos</h1>
     <label> Buscar por:</label>
     <div class="form-inline">
-    
+
         <div class="form-inline my-2">
-      
-        <label for="buscarMateria" style="margin-right: 8px;">Legajo: </label>
+
+            <label for="buscarMateria" style="margin-right: 8px;">Legajo: </label>
             <input type="text" id="buscarMateria" class="form-control" style="margin-right: 8px;">
-            
+
         </div>
         <div class="form-inline my-2">
             <label for="buscarMateria" style="margin-right: 8px;">Nombre: </label>
@@ -23,44 +29,44 @@ include "../header.html";
             <button class="btn btn-outline-primary my-2" id="btnBuscarMateria">Buscar</button>
         </div>
     </div>
-<div class="my-2" style="float:right">
-    <button class="btn btn-success" data-toggle="modal" data-target="#staticBackdrop" >Crear Nuevo</button>
-    <button class="btn btn-success mx-3" data-toggle="modal" data-target="#staticBackdrop1" >Importar Lista</button>
- 
-</div>
+    <div class="my-2" style="float:right">
+        <button class="btn btn-success" data-toggle="modal" data-target="#staticBackdrop">Crear Nuevo</button>
+        <button class="btn btn-success mx-3" data-toggle="modal" data-target="#staticBackdrop1">Importar Lista</button>
+
+    </div>
     <div class="my-5">
         <table class="table table-bordered text-center table-info">
             <thead>
                 <th>Legajo</th>
+                <th>Apellido</th>
                 <th>Nombre </th>
-                <th>Dni</th>
+                <th>DNI</th>
             </thead>
             <tbody>
-                <tr>
-                    <td>8956</td>
-                    <td>Roberto</td>
-                    <td>4578215</td>
-                </tr>
-                <tr>
-                    <td>2356</td>
-                    <td>Mariana</td>
-                    <td>4578129</td>
-                </tr>
-                </tbody>
+                <?php
+                while ($resultado1 = $consulta1->fetch_assoc()) {
+                    echo "<tr>
+                <td>" . $resultado1['legajoAlumno'] . "</td>
+                <td>" . $resultado1['apellidoAlum'] . "</td>
+                <td>" . $resultado1['nombreAlum'] . "</td>
+                <td>" . $resultado1['dniAlum'] . "</td>
+                </tr>";
+                }
+                ?>
+            </tbody>
         </table>
     </div>
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content ">
             <div class="modal-header ">
                 <h5 class="modal-title " id="staticBackdropLabel"> Nuevo Alumno</h5>
             </div>
-            <form method="POST" id="insertAlumno" name="insertAlumno" action="insertAlumno.php" enctype="multipart/form-data" role="form"> 
-                <div class="modal-body"> 
+            <form method="POST" id="insertAlumno" name="insertAlumno" action="insertAlumno.php" enctype="multipart/form-data" role="form">
+                <div class="modal-body">
                     <div class="my-2">
                         <label for="inputName"> Nombre </label>
                         <input type="text" name="inputName" id="inputName" class="form-control" placeholder="Nombre" onchange="validarNombre()" required>
@@ -81,22 +87,21 @@ include "../header.html";
                         <input type="text" name="inputDNI" id="inputDNI" class="form-control" onchange="validarDNI()" onkeydown="return event.keyCode !== 69 && event.keyCode !== 109 && event.keyCode !== 107 && event.keyCode !== 110" placeholder="Documento Nacional de Identidad" required>
                         <h9 class="msg" id="msjValidacionDNI"></h9>
                     </div>
-              
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar </button>
-                <button type="submit" class="btn btn-primary" id="btnCrear"  disabled> Crear </button>
-            </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar </button>
+                    <button type="submit" class="btn btn-primary" id="btnCrear" disabled> Crear </button>
+                </div>
             </form>
-        
+
         </div>
-             
+
     </div>
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="staticBackdrop1" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop1" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content ">
             <div class="modal-header ">
@@ -104,28 +109,27 @@ include "../header.html";
             </div>
             <div class="modal-body">
 
-            <form method="#" id="importPlanilla" name="importPlanilla" action="#" enctype="multipart/form-data" role="form">
-            <div class="container" style="margin-top:50px;">
+                <form method="#" id="importPlanilla" name="importPlanilla" action="#" enctype="multipart/form-data" role="form">
+                    <div class="container" style="margin-top:50px;">
 
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" required name="inpGetFil" id="inpGetFil" accept=".xlsx"
-                        onchange="comprobarListaAlumnos()" lang="es">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" required name="inpGetFil" id="inpGetFil" accept=".xlsx" onchange="comprobarListaAlumnos()" lang="es">
 
-                    <label class="custom-file-label" for="validatedCustomFile">Seleccionar archivo</label>
+                            <label class="custom-file-label" for="validatedCustomFile">Seleccionar archivo</label>
 
-                </div>
-                </div>
-                <!-- la funcion comrobar esta en administrador.js -->
-                <br>
-                <br>
-                <div class="modal-footer">
+                        </div>
+                    </div>
+                    <!-- la funcion comrobar esta en administrador.js -->
+                    <br>
+                    <br>
+                    <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-dismiss="modal"> Cancelar </button>
                         <button type="button" class="btn btn-primary " data-dismiss="modal"> Importar </button>
-                </div>
-    
-        </form>
-    </div>
-            
+                    </div>
+
+                </form>
+            </div>
+
         </div>
     </div>
 </div>
--- signIn/registro.php
+++ signIn/registro.php
@@ -13,37 +13,37 @@ $password = $_POST["password"];
 $fechaNac = $_POST["fechaNac"];
 $rol = $_POST["rol"];
 
-$consulta1 = $con -> query("SELECT * FROM alumno WHERE dniAlum = '$dni' AND legajoAlumno = '$legajo'");
-$resultado1 = $consulta1 -> fetch_assoc();
+$consulta1 = $con->query("SELECT * FROM alumno WHERE dniAlum = '$dni' AND legajoAlumno = '$legajo'");
+$resultado1 = $consulta1->fetch_assoc();
 
 echo "<div class='container'><br>";
 
-if(mysqli_num_rows($consulta1)!=0){
+if (mysqli_num_rows($consulta1) != 0) {
     $id_alumno = $resultado1['id'];
-    $consulta2 = $con -> query("SELECT * FROM permiso WHERE nombrePermiso = UPPER('$rol')");
-    $resultado2 = $consulta2 -> fetch_assoc();
+    $consulta2 = $con->query("SELECT * FROM permiso WHERE nombrePermiso = UPPER('$rol')");
+    $resultado2 = $consulta2->fetch_assoc();
     $id_permiso = $resultado2['id'];
     $fechaActual = date("Y-m-d");
 
-    $actualizacion = $con -> query("UPDATE alumno SET emailAlum = '$email', contraseniaAlum = '$password', fechaNacAlumno = '$fechaNac', 
+    $actualizacion = $con->query("UPDATE alumno SET emailAlum = '$email', contraseniaAlum = '$password', fechaNacAlumno = '$fechaNac', 
     fechaAltaAlumno = '$fechaActual' ,permiso_id = '$id_permiso' WHERE id='$id_alumno'");
-    
-    if($actualizacion){
+
+    if ($actualizacion) {
         echo "<div class='alert alert-success' role='alert'>
         <h3>Se registró correctamente</h3>
       </div>";
-      echo "<a class='btn btn-primary my-2' href='/DayClass/Index.php'>Volver al inicio</a>";
-    } else{
+        echo "<a class='btn btn-primary my-2' href='/DayClass/Index.php'>Volver al inicio</a>";
+    } else {
         echo "<div class='alert alert-danger' role='alert'>
         <h3>Ocurrió un error durante el registro</h3>
       </div>";
-      echo "<a class='btn btn-primary my-2' href='/DayClass/Index.php'>Volver al inicio</a>";
+        echo "<a class='btn btn-primary my-2' href='/DayClass/Index.php'>Volver al inicio</a>";
     }
-} else{
+} else {
     echo "<div class='alert alert-danger' role='alert'>
         <h3>Ocurrió un error durante el registro</h3>
       </div>";
-      echo "<a class='btn btn-primary my-2' href='/DayClass/Index.php'>Volver al inicio</a>";
+    echo "<a class='btn btn-primary my-2' href='/DayClass/Index.php'>Volver al inicio</a>";
 }
 echo "</div>";
 echo "<script>document.getElementById('contenidoNavbar').innerHTML = '';</script>";
