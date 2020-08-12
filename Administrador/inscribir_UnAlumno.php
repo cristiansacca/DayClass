eb9ff3cb8637a0918be9b9d84dde08964c7c3442
Leandro Sgroi <lea220197@gmail.com>
2020-08-12 16:06:02 -0300

    arrgelo y comentarios inscribir un alumno

--- Administrador/inscribir_UnAlumno.php
+++ Administrador/inscribir_UnAlumno.php
@@ -13,43 +13,38 @@ $fchDesde = $resultado1['fechaDesdeCursado'];
 $fchHasta = $resultado1['fechaHastaCursado'];
 
 echo "$fchDesde";
-
+//consultar existencia del alumno (habilitado = fecha de baja null) en la BD  de dayclass
 $consultaAlumID = $con->query("SELECT id FROM `alumno` WHERE (dniAlum = $dni OR legajoAlumno = $legajo) AND fechaBajaAlumno IS NULL");
 
 if(mysqli_num_rows($consultaAlumID) == 0){
-    //alumno inexistente o alumno dado de baja 
+    //si la cosnulta es vacia, el alumno no existe o esta dado de baja, error 2 = alumno inexistente o dado de baja 
     header("Location:/DayClass/Administrador/inscribirAlumnos.php?id=$id_curso&&resultado=2");
-    //echo "el alumno no existe";
+    
     
 }else{
-    echo "el alumno existe";
     $resultado3= $consultaAlumID->fetch_assoc();
     $id_alumno = $resultado3["id"];
     
     echo "$id_alumno";
     echo "$id_curso";
     
-    
+    //verificar que el alumno no vaya a estar inscripto en el ese curso 
     $consultaAlumCursAct = $con->query("SELECT id FROM `alumnocursoactual` WHERE fechaDesdeAlumCurAc = '$fchDesde' AND fechaHastaAlumCurAc = '$fchHasta' AND alumno_id = '$id_alumno' AND curso_id = '$id_curso'");
     
     if(mysqli_num_rows($consultaAlumCursAct) == 0){
-        //insert
+        //si la consulta es vacia, no hay una inscripcion de ese alumno en ese curso en estas fechas actuales
+        
+        //se crea la instancia de inscripcion del alumno
         $resultadoInsertAlumCursAct = $con->query("INSERT INTO `alumnocursoactual`(`fechaDesdeAlumCurAc`, `fechaHastaAlumCurAc`, `alumno_id`, `curso_id`) VALUES ('$fchDesde','$fchHasta','$id_alumno','$id_curso')");
         
+        //se crea la instancia de planilla de asistencia que llevara la cuenta de las asistencias de los alumnos 
         $resultadoInsertAsist = $con->query("INSERT INTO `asistencia`( `alumno_id`, `curso_id`, `fechaHastaFichaAsis`) VALUES ('$id_alumno','$id_curso','$fchHasta')");
         
-        echo "INSERT INTO `alumnocursoactual`(`fechaDesdeAlumCurAc`, `fechaHastaAlumCurAc`, `alumno_id`, `curso_id`) VALUES ('$fchDesde','$fchHasta','$id_alumno','$id_curso')";
-        
-        echo "INSERT INTO `asistencia`( `alumno_id`, `curso_id`, `fechaHastaFichaAsis`) VALUES ('$id_alumno','$id_curso','$fchHasta')";
-        
-        
-        
-        
+        //volver a la pagina que llamo, exito 1 = inscripcion exitosa 
         header("Location:/DayClass/Administrador/inscribirAlumnos.php?id=$id_curso&&resultado=1");
         
     }else{
-        echo "else de ya existe";
-        //alumno ya inscripto en esa materia
+        //error 3 = alumno ya inscripto en esa materia, en ese ciclo lectivo
         header("Location:/DayClass/Administrador/inscribirAlumnos.php?id=$id_curso&&resultado=3");
     }
 }
