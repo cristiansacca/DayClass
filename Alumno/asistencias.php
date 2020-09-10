806895087df06ad07cdef008286cf2a80cf79b0a
cristiansacca <cristiansacca97@gmail.com>
2020-09-10 00:40:09 -0300

    comienzo ver asistencias alumno

--- Alumno/asistencias.php
+++ Alumno/asistencias.php
@@ -13,6 +13,9 @@ if (!isset($_SESSION['alumno']))
 
 ?>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha512-s+xg36jbIujB2S2VKfpGmlC3T5V2TF3lY48DX7u2r9XzGzgPsa6wTpOQA7J9iffvdeBN0q9tKzRxVxw1JviZPg==" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 <div class="container">
     <div class="py-4 my-3 jumbotron bg-light">
         <h1>Información de asistencias</h1>
@@ -48,13 +51,20 @@ if (!isset($_SESSION['alumno']))
         ?>
         
     </div>
-    <h3 class="text-danger">De acá para abajo está hardcodeado</h3>
-    <div class="form-gruup">
-        <label for="" class="mr-2">Cantidad de clases: 4</label><br>
-        <label for="">Faltas disponibles: 8</label><br>
-        <label for="" class="mr-2">Presentes: 2</label><br>
-        <label for="" class="mr-2">Ausentes: 2</label><br>
+
+    <div class="form-inline">
+        <div class="col-md-6 mb-2">
+            <div class="jumbotron py-4">
+                <canvas id="pieChart"></canvas>
+            </div>
+        </div>
+        <div class="col-md-6 mb-2">
+            <div class="jumbotron py-4">
+                <canvas id="barChart"></canvas>
+            </div>
+        </div>
     </div>
+
     <div>
         <table class="table table-bordered text-center table-info table-sm">
             <thead>
@@ -62,34 +72,70 @@ if (!isset($_SESSION['alumno']))
                 <th>Asistencia</th>
                 <th>Justificado</th>
             </thead>
-            <tbody>
-                <tr>
-                    <td>Lunes 25/04</td>
-                    <td class="text-success">Presente</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td>Lunes 25/04</td>
-                    <td class="text-success">Presente</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td>Lunes 25/04</td>
-                    <td class="text-danger">Ausente</td>
-                    <td>Sí</td>
-                </tr>
-                <tr>
-                    <td>Lunes 25/04</td>
-                    <td class="text-danger">Ausente</td>
-                    <td>No</td>
-                </tr>
+            <tbody id="tablaAsistencias">
+
             </tbody>
         </table>
     </div>
 </div>
 
 <script src="alumno.js"></script>
+<script>
+    document.getElementById("materias").onchange = function () {
 
+    }
+
+    function mostrarAsistencias(id_curso) {
+
+    }
+    var ctx = document.getElementById('pieChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Presentes', 'Ausentes', 'Justificados'],
+            datasets: [{
+                label: 'Asistencias vs. Inasistencias vs. Justificados',
+                data: [25,5,2],
+                backgroundColor: ['rgba(0, 147, 0, 0.5)', 'rgba(255, 99, 132, 0.5)', 'rgba(255, 255, 0, 0.5)'],
+                borderColor: ['rgba(0, 147, 0, 1)','rgba(255, 99, 132, 1)', 'rgba(218, 165, 32, 1)'],
+                borderWidth: 1.5
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    var ctx2 = document.getElementById('barChart').getContext('2d');
+    var myChart2 = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ['Presentes', 'Ausentes', 'Justificados'],
+            datasets: [{
+                label: 'Asistencias vs. Inasistencias vs. Justificados',
+                data: [25,5,2],
+                backgroundColor: ['rgba(0, 147, 0, 0.5)', 'rgba(255, 99, 132, 0.5)', 'rgba(255, 255, 0, 0.5)'],
+                borderColor: ['rgba(0, 147, 0, 1)','rgba(255, 99, 132, 1)', 'rgba(218, 165, 32, 1)'],
+                borderWidth: 1.5
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+</script>
 <?php
 include "modal-autoasistencia.php";
 ?>
