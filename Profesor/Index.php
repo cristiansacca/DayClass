2e6bd4769f7af2a589b3573c028ff577ebd4b563
Leandro Sgroi <lea220197@gmail.com>
2020-09-11 21:50:07 -0300

    modificaciones en index profesor

--- Profesor/Index.php
+++ Profesor/Index.php
@@ -37,34 +37,46 @@ if($hora >= date('06:00:00') && $hora < date('12:00:00')) {
 
 
         <?php
-        $contador = 0;
+        
         $id_prof = $_SESSION['profesor']['id'];
-        $consultaCargo = $con->query("SELECT * FROM cargoprofesor WHERE profesor_id= '$id_prof'");
-
-        while ($cargos = $consultaCargo->fetch_assoc()) {
-            if ($contador == 4) {
-                $contador = 0;
-            }
+        date_default_timezone_set('America/Argentina/Buenos_Aires');
+        $currentDateTime = date('Y-m-d');
+        $consultaCargo = $con->query("SELECT * FROM cargoprofesor WHERE profesor_id= '$id_prof' AND cargoprofesor.fechaDesdeCargo <= '$currentDateTime' AND cargoprofesor.fechaHastaCargo IS NULL");
+        
             
-            $consultaCursos = $con->query("SELECT * FROM curso WHERE id='" . $cargos['curso_id'] . "'");
-            $resultadoCursos = $consultaCursos->fetch_assoc();
-            $cargoProf = $con->query("SELECT * FROM cargo WHERE id='" . $cargos['cargo_id'] . "'")->fetch_assoc();
+        if(mysqli_num_rows($consultaCargo)>0){
+            $contador = 0;
+            while ($cargos = $consultaCargo->fetch_assoc()) {
+                if ($contador == 4) {
+                    $contador = 0;
+                }
 
-            echo "<div class='col-lg-6 col-md-3 mb-4' >
-                <div class='card color$contador' >
-                    <div class='card-body'>
-                        <h4 class='card-title'>".$resultadoCursos["nombreCurso"]."</h4>
-                        <h5 class='font-weight-normal'>".$cargoProf['nombreCargo']."</h5>
+                $consultaCursos = $con->query("SELECT * FROM curso WHERE id='" . $cargos['curso_id'] . "' AND fechaHastaCurActul IS NULL");
+                $resultadoCursos = $consultaCursos->fetch_assoc();
+                $cargoProf = $con->query("SELECT * FROM cargo WHERE id='" . $cargos['cargo_id'] . "'")->fetch_assoc();
+
+                echo "<div class='col-lg-6 col-md-3 mb-4' >
+                    <div class='card color$contador' >
+                        <div class='card-body'>
+                            <h4 class='card-title'>".$resultadoCursos["nombreCurso"]."</h4>
+                            <h5 class='font-weight-normal'>".$cargoProf['nombreCargo']."</h5>
+                        </div>
+                        <div class='card-footer'>
+                            <a href='indexCurso.php?id_curso=".$resultadoCursos["id"]."' class='btn btn-primary btn-lg'>Ingresar</a>
+                        </div>
                     </div>
-                    <div class='card-footer'>
-                        <a href='indexCurso.php?id_curso=".$resultadoCursos["id"]."' class='btn btn-primary btn-lg'>Ingresar</a>
-                    </div>
-                </div>
+                </div>";
+
+                $contador++;
+            }
+
+        }else{
+             echo "<div class='alert alert-warning' role='alert'>
+                <h5>Todavia no lo han asignado a un curso.</h5>
             </div>";
-
-            $contador++;
         }
 
+        
         ?>
 
     </div>
