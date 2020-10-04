bd93202565bc57b4d3bb2000f78fbfb0c2bf2223
cristiansacca <cristiansacca97@gmail.com>
2020-10-04 18:45:44 -0300

    Fecha backup

--- Administrador/ConfiguracionSistema/BackupRecuperacion/backup.php
+++ Administrador/ConfiguracionSistema/BackupRecuperacion/backup.php
@@ -71,7 +71,7 @@ include "../../../databaseConection.php";
                 echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
                             <h5><i class='fa fa-exclamation-circle mr-2'></i>No hay ninguna copia almacenada para restaurar.</h5>";
                 break;
-             case 6:
+            case 6:
                 echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                             <h5><i class='fa fa-exclamation-circle mr-2'></i>Error del sistema. Contante al administrador de base de datos, para restauración manual.</h5>";
                 break;
@@ -85,48 +85,50 @@ include "../../../databaseConection.php";
     ?>
 
     <div class="mt-4">
-        <h5>Última copia de seguridad generada</h5> 
+        <h5>Última copia de seguridad generada</h5>
         <table class="table table-bordered bg-light">
             <tr>
-                
-                <td><?php 
+
+                <td><b>Archivo: </b><?php
                     $files = glob('backupDB/*'); //obtenemos todos los nombres de los ficheros
                     $archivo = null;
 
-                    if(count($files) !== 0){
-                        foreach($files as $file){
-                            if(is_file($file))
-                            //echo $file;
-                            
-                            $archivo = strval($file);
-                            $archivo = explode("/",$archivo);
+                    if (count($files) !== 0) {
+                        foreach ($files as $file) {
+                            if (is_file($file))
+                                //echo $file;
+
+                                $archivo = strval($file);
+                            $archivo = explode("/", $archivo);
                             $nombreArchivo = $archivo[1];
                             echo $nombreArchivo;
-                         }
-                    }else{
+                        }
+                    } else {
                         echo "No se ha generado ningún backup";
-                    }?></td>
-                <td>Fecha: <?php 
-                    
-                    $fechaArchivo = substr($nombreArchivo, 12,17);
-                    
-                   echo strftime("%d / %m / %Y", strtotime($fechaArchivo));
-                   
-
-                    ?></td>
+                    } ?></td>
+                <td><b>Fecha:</b> 
+                    <?php
+                        if (count($files) !== 0) {
+                            $fechaArchivo = date_create_from_format ("Y-m-d-His", substr($nombreArchivo, 12, 17));
+                            echo date_format($fechaArchivo, "d/m/Y H:i:s");   
+                        } else {
+                            echo "No se ha generado ningún backup";
+                        }    
+                    ?>
+                </td>
             </tr>
         </table>
     </div>
-    
-    
+
+
     <div class="mt-4">
-      <h5>Generar copia de seguridad</h5> 
+        <h5>Generar copia de seguridad</h5>
         <a href="descargaBackup.php" class="btn btn-primary"><i class="fa fa-database mr-1"></i>Realizar copia</a>
-    <a href="descargaBackup.php?download=true" class="btn btn-secondary"><i class="fa fa-download mr-1"></i>Descargar copia</a>
+        <a href="descargaBackup.php?download=true" class="btn btn-secondary"><i class="fa fa-download mr-1"></i>Descargar copia</a>
     </div>
-    
+
     <div class="mt-4">
-        <h5>Restaurar base de datos anterior</h5> 
+        <h5>Restaurar base de datos anterior</h5>
         <button class="btn btn-success" data-toggle="modal" data-target="#restoreDataBase"><i class="fa fa-upload mr-1"></i>Restaurar base de datos</button>
     </div>
 
@@ -140,12 +142,12 @@ include "../../../databaseConection.php";
             <div class="modal-header ">
                 <h3 class="modal-title " id="staticBackdropLabel">Recuperación de base de datos</h3>
             </div>
-            
-             <form method="POST" id="uploadBackup" name="uploadBackup" action="restaurarBackup.php" enctype="multipart/form-data" role="form">
-            
+
+            <form method="POST" id="uploadBackup" name="uploadBackup" action="restaurarBackup.php" enctype="multipart/form-data" role="form">
+
                 <div class="modal-body">
-                <h9>Seleccione la forma de recuperación:</h9>
-                    
+                    <h9>Seleccione la forma de recuperación:</h9>
+
                     <div class="container">
                         <div class="radio">
                             <label onclick='hide()'><input type="radio" id="guardado" name="optradio" checked onclick='hide()'> Copia guadada en el sistema.</label>
@@ -155,27 +157,27 @@ include "../../../databaseConection.php";
                             <label onclick='unHide()'><input type="radio" id="subir" name="optradio" onclick='unHide()'> Subir copia guadada.</label>
                         </div>
                     </div>
-                    
-                        <div class="container" name="msgLastBackUp" id="msgLastBackUp">
-                            <table class="table table-bordered bg-light">
-                                <tr>
-                                    <td>Copia de seguridad que se va a restaurar:</td>
-                                    <td><?php 
-                                        
-                                        $files = glob('backupDB/*'); //obtenemos todos los nombres de los ficheros
 
-                                        if(count($files) !== 0){
-                                            foreach($files as $file){
-                                                if(is_file($file))
+                    <div class="container" name="msgLastBackUp" id="msgLastBackUp">
+                        <table class="table table-bordered bg-light">
+                            <tr>
+                                <td>Copia de seguridad que se va a restaurar:</td>
+                                <td><?php
+
+                                    $files = glob('backupDB/*'); //obtenemos todos los nombres de los ficheros
+
+                                    if (count($files) !== 0) {
+                                        foreach ($files as $file) {
+                                            if (is_file($file))
                                                 echo $file;
-                                             }
-                                        } else {
-                                            echo nl2br ("<b> No hay ningún backup guardado en el sistema.\n Revise si tiene copias locales descaragdas.</b>");
-                                        }?></td>
-                                </tr>
-                            </table>
-                        </div>
-                    
+                                        }
+                                    } else {
+                                        echo nl2br("<b> No hay ningún backup guardado en el sistema.\n Revise si tiene copias locales descaragdas.</b>");
+                                    } ?></td>
+                            </tr>
+                        </table>
+                    </div>
+
                     <div class="container" name="inputFile" id="inputFile" style="display:none">
                         <div class="custom-file">
                             <input type="file" class="form-control-file" id="inputSQL" name="inputSQL" placeholder="base de datos que deseas restaurar" accept=".sql" value="">
@@ -188,7 +190,7 @@ include "../../../databaseConection.php";
                 </div>
 
             </form>
-            
+
         </div>
     </div>
 </div>
