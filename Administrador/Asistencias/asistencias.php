8f852767b0ae65569e4209af6f0e1e5b12e2b4b1
Cristian Sacca <cristiansacca97@gmail.com>
2020-10-30 01:36:32 -0300

    Ver asistencias

--- Administrador/Asistencias/asistencias.php
+++ Administrador/Asistencias/asistencias.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['administrador'])) {
 }
 
 //Comprobamos si esta definida la sesión 'tiempo'.
-if (isset($_SESSION['tiempo']) && isset($_SESSION['limite'])) {
+/*if (isset($_SESSION['tiempo']) && isset($_SESSION['limite'])) {
 
     //Calculamos tiempo de vida inactivo.
     $vida_session = time() - $_SESSION['tiempo'];
@@ -29,8 +29,8 @@ if (isset($_SESSION['tiempo']) && isset($_SESSION['limite'])) {
         exit();
     }
 }
-$_SESSION['tiempo'] = time();
-  
+$_SESSION['tiempo'] = time();*/
+
 ?>
 
 <div class="container">
@@ -38,27 +38,193 @@ $_SESSION['tiempo'] = time();
         <h1>Asistencias</h1>
         <a href="/DayClass/Administrador/index.php" class="btn btn-info"><i class="fa fa-arrow-circle-left mr-1"></i>Volver</a>
     </div>
-    <div class="row mx-1">
-        <select name="materia" id="materia" class="custom-select col-md-6">
-            <option value="">Materias</option>
-            <?php
-                $consultaMateria = $con->query("SELECT * FROM materia WHERE fechaBajaMateria IS NULL ORDER BY nombreMateria, nivelMateria ASC");
-
-                while($materia = $consultaMateria->fetch_assoc()){
-                    echo "<option value='".$materia['id']."'>".$materia['nombreMateria']." (Nivel ".$materia['nivelMateria'].")</option>";
-                }
-
-            ?>
-        </select>
-        <select name="curso" id="curso" class="custom-select col-md-6">
-            <option value="">Cursos</option>
-        </select>
+    <div class="my-3">
+        <h4 class="font-weight-normal">Seleccione una materia, un curso y una fecha para ver las asistencias.</h4>
     </div>
+    <div class="row mb-2">
+        <div class="col-md-4 mb-2">
+            <label for="materia">Materia:</label>
+            <select name="materia" id="materia" class="custom-select">
+                <option value="">Materias</option>
+                <?php
+                $consultaMateria = $con->query("SELECT * FROM materia WHERE fechaBajaMateria IS NULL ORDER BY nombreMateria, nivelMateria ASC");
+            
+                while ($materia = $consultaMateria->fetch_assoc()) {
+                    echo "<option value='" . $materia['id'] . "'>" . $materia['nombreMateria'] . " (Nivel " . $materia['nivelMateria'] . ")</option>";
+                }
+            
+                ?>
+            </select>
+        </div>
+        <div class="col-md-4 mb-2">
+            <label for="curso">Curso:</label>
+            <select name="curso" id="curso" class="custom-select" disabled>
+                <option value="">Cursos</option>
+            </select>
+        </div>
+        <div class="col-md-4 mb-2">
+            <label for="fecha">Fecha:</label>
+            <input type="date" name="fecha" id="fecha" class="form-control" disabled>
+        </div>
+    </div>
+
+    <div class="mb-4" id="sinAsistencias" hidden>
+        <div class="alert alert-danger" role="alert">
+            <h5><i class='fa fa-exclamation-circle mr-2'></i>No se registran asistencias para la fecha seleccionada.</h5>
+        </div>
+        <form action="#" method="POST">
+            <input type="number" name="idCursoCargar" hidden>
+            <input type="date" name="fechaCargar" hidden>
+            <button type="submit" class="btn btn-primary"><i class="fa fa-tasks mr-1"></i>Cargar asistencia</button>
+        </form>
+    </div>
+
+    <div id="tablaAsistencia" class="table-responsive" hidden>
+        <form action="#" method="POST">
+            <input type="number" name="idCursoEditar" hidden>
+            <input type="date" name="fechaEditar" hidden>
+            <button type="submit" class="btn btn-success my-3"><i class="fa fa-edit mr-1"></i>Editar asistencias</button>
+        </form>
+        <table class="table table-secondary table-bordered">
+            <thead>
+                <th>Legajo</th>
+                <th>Apellido</th>
+                <th>Nombre</th>
+                <th>Estado</th>
+            </thead>
+            <tbody id="tbodyAsistencia">
+
+            </tbody>
+        </table>
+    </div>
+
 </div>
 
 <script src="../administrador.js"></script>
 <script>
     <?php echo "document.getElementById('nombreUsuarioNav').innerHTML = '" . $_SESSION['administrador']['nombreAdm'] . " " . $_SESSION['administrador']['apellidoAdm'] . "'" ?>
+</script>
+
+<script>
+    document.getElementById("fecha").onchange = function() {
+        var fecha = document.getElementById("fecha").value;
+        var id_curso = document.getElementById("curso").value;
+        var datos = {
+            id_curso: id_curso,
+            fecha: fecha
+        }
+
+        $.ajax({
+            url: 'obtenerAsistencia.php',
+            type: 'POST',
+            data: datos,
+            success: function(datosRecibidos) {
+                //alert(datosRecibidos);
+                json = JSON.parse(datosRecibidos);
+                var contenido = "";
+                if (json.length != 0) {
+                    for (let index = 0; index < json.length; index++) {
+                        var color;
+                        if(json[index].estado == "PRESENTE"){
+                            color = 'text-success';
+                        } else {
+                            if(json[index].estado == "AUSENTE"){
+                                color = 'text-danger';
+                            } else {
+                                color = 'text-warning';
+                            }
+                        }
+                        contenido+="<tr>";
+                        contenido += "<td>"+json[index].legajo+"</td>";
+                        contenido += "<td>"+json[index].apellido+"</td>";
+                        contenido += "<td>"+json[index].nombre+"</td>";
+                        contenido += "<td class='"+color+"'><b>"+json[index].estado+"</b></td>";
+                        contenido += "</tr>";
+                    }
+                    document.getElementById("tbodyAsistencia").innerHTML = contenido;
+                    document.getElementById("sinAsistencias").hidden = true;
+                    document.getElementById("tablaAsistencia").hidden = false;
+                    document.getElementById("idCursoEditar").value = id_curso;
+                    document.getElementById("fechaEditar").value = fecha;
+                                        
+                } else {
+                    document.getElementById("tbodyAsistencia").innerHTML = contenido;
+                    document.getElementById("tablaAsistencia").hidden = true;
+                    document.getElementById("sinAsistencias").hidden = false;
+                    document.getElementById("idCursoCargar").value = id_curso;
+                    document.getElementById("fechaCargar").value = fecha;
+                }
+
+            }
+        })
+    }
+
+    document.getElementById("materia").onchange = function() {
+        $("#fecha").attr("disabled", "disabled");
+        $("#curso").attr("disabled", "disabled");
+        document.getElementById("tablaAsistencia").hidden = true;
+        document.getElementById("tbodyAsistencia").innerHTML = "";
+        var id_materia = document.getElementById("materia").value;
+        var datos = {
+            id_materia: id_materia
+        }
+
+        $.ajax({
+            url: '/DayClass/Administrador/Estadisticas/listarCursos.php',
+            type: 'POST',
+            data: datos,
+            success: function(datosRecibidos) {
+                //alert(datosRecibidos);
+                json = JSON.parse(datosRecibidos);
+                var contenido;
+                if (json.length != 0) {
+                    contenido = "<option value='' selected>Seleccione</option>";
+                    for (let index = 0; index < json.length; index++) {
+                        contenido += "<option value='" + json[index].id + "'>" + json[index].nombreCurso + "</option>";
+                        document.getElementById("curso").innerHTML = contenido;
+                        $("#curso").removeAttr("disabled");
+                    }
+                } else {
+                    contenido = "<option value='' selected>No hay cursos</option>";
+                    document.getElementById("curso").innerHTML = contenido;
+                    $("#curso").attr("disabled", "disabled");
+                }
+
+            }
+        })
+    }
+
+    document.getElementById("curso").onchange = function() {
+        var id_curso = document.getElementById("curso").value;
+        var datos = {
+            id_curso: id_curso
+        }
+        if (id_curso != "") {
+            $.ajax({
+                url: 'datosCurso.php',
+                type: 'POST',
+                data: datos,
+                success: function(datosRecibidos) {
+                    //alert(datosRecibidos);
+                    json = JSON.parse(datosRecibidos);
+                    var fechaDesde = json.fechaDesdeCursado;
+                    var fechaHasta = json.fechaHastaCursado;
+                    var fechaActual = json.fechaActual;
+                    if(fechaDesde != null && fechaHasta != null){
+                        document.getElementById("fecha").min = fechaDesde;
+                        if(fechaHasta < fechaActual){
+                            document.getElementById("fecha").max = fechaHasta;
+                        } else {
+                            document.getElementById("fecha").max = fechaActual;
+                        }
+                        $("#fecha").removeAttr("disabled");
+                    }
+                }
+            })
+        } else {
+            $("#fecha").attr("disabled", "disabled");
+        }
+    }
 </script>
 
 <?php
---
+++ Administrador/Asistencias/obtenerAsistencia.php
@@ -0,0 +1,34 @@
+<?php 
+include "../../databaseConection.php";
+
+$id_curso = $_POST['id_curso'];
+$fecha = $_POST['fecha'];
+$fecha1 = $fecha.'  00:00:00';
+$fecha2 = $fecha.'  23:59:59';
+
+$consultaAsistencias = $con->query("SELECT alumno.apellidoAlum, alumno.nombreAlum, alumno.legajoAlumno, asistenciadia.tipoAsistencia_id, tipoasistencia.nombreTipoAsistencia AS estado 
+FROM `asistenciadia`, `alumno`, `asistencia`, `tipoasistencia` 
+WHERE asistencia.curso_id = '$id_curso' 
+    AND asistencia.alumno_id = alumno.id 
+    AND asistenciadia.fechaHoraAsisDia > '$fecha1' 
+    AND asistenciadia.fechaHoraAsisDia < '$fecha2' 
+    AND asistenciadia.asistencia_id = asistencia.id
+    AND asistenciadia.tipoAsistencia_id = tipoasistencia.id
+    ORDER BY alumno.apellidoAlum ASC");
+
+$asistencias = array();
+while($resultado1 = $consultaAsistencias->fetch_assoc()) {
+    $asistencias[] = array(
+        'legajo'=> $resultado1['legajoAlumno'],
+        'apellido'=> $resultado1['apellidoAlum'],
+        'nombre' => $resultado1['nombreAlum'],
+        'tipoAsistencia' => $resultado1['tipoAsistencia_id'],
+        'estado' => $resultado1['estado']
+    );
+}
+
+$myJSON = json_encode($asistencias);
+
+echo $myJSON;
+
+?>
