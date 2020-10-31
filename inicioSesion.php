4f3a29a8368873d7b727405b00dca0e0188f29c4
Leandro Sgroi <lea220197@gmail.com>
2020-10-30 21:09:51 -0300

    signin

--- inicioSesion.php
+++ inicioSesion.php
@@ -2,13 +2,25 @@
 //Se inicia o restaura la sesión
 session_start();
 
-include "../header.html";
-include "../databaseConection.php";
+include "header.html";
+include "databaseConection.php";
+
+$nombreRol = "Sin rol asignado";
 
 //Si la variable sesión está vacía es porque no se ha iniciado sesión
-if (!isset($_SESSION['profesor'])) {
+if (!isset($_SESSION['usuario'])) {
     //Nos envía a la página de inicio
     header("location:/DayClass/index.php");
+}else{
+    
+   if( $_SESSION["usuario"]["id_permiso"] != NULL || $_SESSION["usuario"]["id_permiso"] != ""){
+       $selectRol = $con->query("SELECT * FROM permiso WHERE permiso.id = '".$_SESSION["usuario"]["id_permiso"]."'");
+       $rol = $selectRol->fetch_assoc();
+       $nombreRol = $rol["nombrePermiso"];
+   }else{
+       
+   }
+  
 }
 
 //Comprobamos si esta definida la sesión 'tiempo'.
@@ -49,51 +61,26 @@ if($hora >= date('06:00:00') && $hora < date('12:00:00')) {
 <div class="container">
 
     <div class="jumbotron my-4 py-4">
-        <h1><?php echo "$saludo, " . $_SESSION["profesor"]["nombreUsuario"] /*. " " . $_SESSION["profesor"]["apellidoProf"]*/ ?></h1>
+        <h6>Rol: <?php echo "$nombreRol" ?></h6>
+        <h1><?php echo "$saludo, " . $_SESSION["usuario"]["nombreUsuario"]?></h1>
         <a href="editar_perfil.php" class="btn btn-success"><i class="fa fa-edit mr-1"></i>Editar Perfil</a>
     </div>
+    
     <h3 class="font-weight-normal">Accesos asignados:</h3><br>
     <!-- Page Features -->
     <div class="row text-center">
-
-
         <?php
-        
-        $id_prof = $_SESSION['profesor']['id'];
+    
+        if($_SESSION["usuario"]["id_permiso"] != NULL || $_SESSION["usuario"]["id_permiso"] != ""){
+            
+        $id_usuario = $_SESSION['usuario']['id'];
         date_default_timezone_set('America/Argentina/Buenos_Aires');
         $currentDateTime = date('Y-m-d');
-        $consultaCargo = $con->query("SELECT * FROM cargoprofesor WHERE profesor_id= '$id_prof' AND cargoprofesor.fechaDesdeCargo <= '$currentDateTime' AND cargoprofesor.fechaHastaCargo IS NULL");
-        
             
-        if(mysqli_num_rows($consultaCargo)>0){
-            $contador = 0;
-            while ($cargos = $consultaCargo->fetch_assoc()) {
-                if ($contador == 4) {
-                    $contador = 0;
-                }
-
-                $consultaCursos = $con->query("SELECT * FROM curso WHERE id='" . $cargos['curso_id'] . "' AND fechaHastaCurActul IS NULL");
-                $resultadoCursos = $consultaCursos->fetch_assoc();
-                $cargoProf = $con->query("SELECT * FROM cargo WHERE id='" . $cargos['cargo_id'] . "'")->fetch_assoc();
-
-                echo "<div class='col-lg-6 col-md-3 mb-4' >
-                    <div class='card color$contador' >
-                        <div class='card-body'>
-                            <h4 class='card-title'>".$resultadoCursos["nombreCurso"]."</h4>
-                            <h5 class='font-weight-normal'>".$cargoProf['nombreCargo']."</h5>
-                        </div>
-                        <div class='card-footer'>
-                            <a href='indexCurso.php?id_curso=".$resultadoCursos["id"]."' class='btn btn-primary btn-lg'>Ingresar</a>
-                        </div>
-                    </div>
-                </div>";
-
-                $contador++;
-            }
-
+        
         }else{
              echo "<div class='alert alert-warning' role='alert'>
-                <h5><i class='fa fa-exclamation-circle mr-2'></i>Todavia no lo han asignado a un curso.</h5>
+                <h5><i class='fa fa-exclamation-circle mr-2'></i>Todavia no tiene rol asignado.</h5>
             </div>";
         }
 
--- login.php
+++ login.php
@@ -10,7 +10,7 @@ $email = $_POST["email"];
 $contrasenia = $_POST["contrasenia"];
 
 //Realiza las consultas con el mail ingresado para obtener el usuario
-$consulta1 = $con->query("SELECT * FROM alumno WHERE emailAlum = '$email'");
+$consulta1 = $con->query("SELECT * FROM usuario WHERE emailUsuario = '$email'");
 $consultaSesion = $con->query("SELECT * FROM vigenciasesion");
 
 //Si el tiempo de sesion no está definido por defecto se ponen 40 minutos
@@ -21,99 +21,43 @@ if(($consultaSesion->num_rows) == 0){
 }
 
 if (($consulta1->num_rows) == 1) { //Si la consulta 1 obtiene un resultado verifica la contraseña
-    
     $resultado1 = $consulta1->fetch_assoc();
-    $fechaBajaAlumno = $resultado1["fechaBajaAlumno"];
+    $resultado1 = $resultado1["cuentaHabilitada"];
     
-    if($fechaBajaAlumno == "" || $fechaBajaAlumno == null){//validar que la cuenta no haya sido dada de baja 
-        $cifrada = $resultado1["contraseniaAlum"];
-        if (password_verify($contrasenia, $cifrada)) {
-            //Si la contraseña cifrada coincide con lo ingresado se inicia la sesión
-            session_start();
-            //En la variable de sesión se guardan los datos del usuario que ingresó
-            $_SESSION["alumno"] = $resultado1;
-
-            //Se define la variable de sesión con el tiempo límite de inactividad en minutos
-            $_SESSION['limite'] = ($limiteSesion*60);
-
-            //Se redirigue a la página principal correspondiente al usuario
-            header("Location: /DayClass/Alumno/index.php");
-
-        } else {
-            header("Location: /DayClass/index.php?error=0");
-        }
-        
-    }else{
-        if($fechaBajaAlumno <= $currentDate){
-            header("Location: /DayClass/index.php?error=2");
-        }
-    }
-} else {
-
-    $consulta2 = $con->query("SELECT * FROM profesor WHERE emailProf = '$email'");
-
-    if (($consulta2->num_rows) == 1) {
-        
-        $resultado2 = $consulta2->fetch_assoc();
-        
-        $fechaBajaProf = $resultado2["fechaBajaProf"];
     
-        if($fechaBajaProf == "" || $fechaBajaProf == null){ //validar que la cuenta no haya sido dada de baja 
-        
-            $cifrada = $resultado2["contraseniaProf"];
+    if($cuentaHabilitada == 1){
+        $fechaBajaUsuario = $resultado1["fechaBajaUsuario"];
 
+        if($fechaBajaUsuario == "" || $fechaBajaUsuario == null){//validar que la cuenta no haya sido dada de baja 
+            $cifrada = $resultado1["contraseniaUsuario"];
             if (password_verify($contrasenia, $cifrada)) {
-
+                //Si la contraseña cifrada coincide con lo ingresado se inicia la sesión
                 session_start();
-                $_SESSION["profesor"] = $resultado2;
+                //En la variable de sesión se guardan los datos del usuario que ingresó
+                $_SESSION["usuario"] = $resultado1;
+
                 //Se define la variable de sesión con el tiempo límite de inactividad en minutos
                 $_SESSION['limite'] = ($limiteSesion*60);
-                header("Location: /DayClass/Profesor/index.php");
+
+                //Se redirigue a la página principal correspondiente al usuario
+                header("Location: /DayClass/inicioSesion.php");
 
             } else {
                 header("Location: /DayClass/index.php?error=0");
             }
+
         }else{
-            if($fechaBajaProf <= $currentDate){
+            if($fechaBajaUsuario <= $currentDate){
                 header("Location: /DayClass/index.php?error=2");
-            }  
-        }
-    }else{
-
-        $consulta3 = $con->query("SELECT * FROM administrativo WHERE emailAdm = '$email'");
-
-        if (($consulta3->num_rows) == 1) {
-            
-            $resultado3 = $consulta3->fetch_assoc();
-            
-            $fechaBajaAdmin = $resultado3["fechaBajaAdm"];
-            
-            if($fechaBajaAdmin == "" || $fechaBajaAdmin == null){//validar que la cuenta no haya sido dada de baja 
-                
-                $cifrada = $resultado3["contraseniaAdm"];
-
-                if (password_verify($contrasenia, $cifrada)) {
-
-                    session_start();
-                    $_SESSION["administrador"] = $resultado3;
-                    //Se define la variable de sesión con el tiempo límite de inactividad en minutos
-                    $_SESSION['limite'] = ($limiteSesion*60);
-                    header("Location: /DayClass/Administrador/index.php");
-
-                } else {
-                    header("Location: /DayClass/index.php?error=0");
-                }
-            
-            }else{
-                if($fechaBajaAdmin <= $currentDate){
-                    header("Location: /DayClass/index.php?error=2");
-                } 
             }
-        } else { //Si ninguna consulta obtnien resultado el email ingresado no existe en la base de datos
-            header("Location: /DayClass/index.php?error=1");
         }
+    
+    }else{
+        header("Location: /DayClass/index.php?error=3");
     }
-    }
+}else{
+    header("Location: /DayClass/index.php?error=1");
+}
 
 
 ?>
