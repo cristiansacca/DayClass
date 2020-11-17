88737d002753559fef453ae3fcd2adb57e3e53ee
Cristian Sacca <cristiansacca97@gmail.com>
2020-11-17 16:12:14 -0300

    Correcciones en publicar novedades

--- Profesor/PizarraNovedades/insertPublicacion.php
+++ Profesor/PizarraNovedades/insertPublicacion.php
@@ -16,9 +16,11 @@ $asunto = $_POST["inputAsunto"];
 $mensaje= $_POST["textMensaje"];
 $fechaHoraNotif = date('Y-m-d H:i:s');
 $id_curso = $_POST["id_curso"];
+
+$curso = $con->query("SELECT * FROM curso WHERE id = '$id_curso'")->fetch_assoc();
     
-$insert = $con->query("INSERT INTO notificacionprofe (asunto, mensaje, fechaHoraNotif, profesor_id, curso_id)
- VALUES ('$asunto', '$mensaje', '$fechaHoraNotif', '".$_SESSION['usuario']['id']."', '$id_curso')");
+$insert = $con->query("INSERT INTO notificacionprofe (asunto, mensaje, fechaHoraNotif, profesor_id, curso_id, fechaDesdeNotificacionProfe, fechaHastaNotificacionProfe)
+ VALUES ('$asunto', '$mensaje', '$fechaHoraNotif', '".$_SESSION['usuario']['id']."', '$id_curso', '".$curso['fechaDesdeCursado']."', '".$curso['fechaHastaCursado']."')");
 
 if($insert){//Si se insertó correctamente devuelve 1, sino devuelve 0. Para mostrar los mensajes correspondientes.
     header("location: /DayClass/Profesor/PizarraNovedades/pizarra.php?id_curso=$id_curso&&resultado=1");
