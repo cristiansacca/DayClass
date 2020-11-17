6e1c6c6c93b40ed778178db4b11b54ee6bc4015f
Leandro Sgroi <lea220197@gmail.com>
2020-11-17 15:40:27 -0300

    cambios lean

--- Profesor/PizarraNovedades/insertPublicacion.php
+++ Profesor/PizarraNovedades/insertPublicacion.php
@@ -18,7 +18,7 @@ $fechaHoraNotif = date('Y-m-d H:i:s');
 $id_curso = $_POST["id_curso"];
     
 $insert = $con->query("INSERT INTO notificacionprofe (asunto, mensaje, fechaHoraNotif, profesor_id, curso_id)
- VALUES ('$asunto', '$mensaje', '$fechaHoraNotif', '".$_SESSION['profesor']['id']."', '$id_curso')");
+ VALUES ('$asunto', '$mensaje', '$fechaHoraNotif', '".$_SESSION['usuario']['id']."', '$id_curso')");
 
 if($insert){//Si se insertó correctamente devuelve 1, sino devuelve 0. Para mostrar los mensajes correspondientes.
     header("location: /DayClass/Profesor/PizarraNovedades/pizarra.php?id_curso=$id_curso&&resultado=1");
